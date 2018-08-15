8f3165e0518ced59cc83bc3083f42dd5fb1d12f2
aerfa <503773902@qq.com>
2018-08-15 17:30:24 +0800

    新增web框架

---
+++ web-temp/src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import "babel-polyfill"
+
+//调用bootstrap
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
---
+++ web-temp/src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="page">
+        <b-alert show>Default Alert</b-alert>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
