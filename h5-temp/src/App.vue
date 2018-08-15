a3c087aa363d4a8f352b931d501f5cae1b2af055
aerfa <503773902@qq.com>
2018-08-15 14:51:39 +0800

    更新H5项目框架

---
+++ h5-temp/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+@import './assets/scss/vux.scss';
+#app,.page {
+  width: 100%;
+  height: 100%;
+}
+</style>
